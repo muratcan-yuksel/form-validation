adec5d65f23d43b21a0e8546319e32d4c3de1116
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-29 23:46:49 +0300

    done with the email

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //get the username
-const userName= document.getElementById("userName");
+    const userName= document.getElementById("userName");
+    //get the error message
     const userError= document.getElementById("userError");
-
+//validate the username
 function userInput () {
     if( userName.value.length<5 || userName.value.length>15){
         userError.textContent= "User name must be between 5-15 characters long";
@@ -9,7 +10,16 @@ function userInput () {
         userError.textContent="";
     }
 }
-    // function removeError () {
+    function emailInput() {
+        const email= document.getElementById("email");
+        //regex for sth@sth.sth
+        const reg= /\S+@\S+\.\S+/;
+        //get email error
+        const error= document.getElementById("emailError");
+            if (reg.test(email.value)==false){
+            error.textContent= "Please enter a valid email address"
+            }else{
+                error.textContent="";
+            }
 
-     
-    // }
+    }
